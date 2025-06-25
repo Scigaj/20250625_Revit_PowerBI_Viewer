54e327123105e91de7ff9a9c20878f9e756cf91b
Michal Scigaj <33829411+Scigaj@users.noreply.github.com>
2025-06-25 21:04:45 +0100

    Revit Plugin Update

---
+++ Revit_PowerBI_Viewer/Revit_PowerBI_Viewer/Application.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Autodesk.Revit.UI;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using System.IO;
+
+
+
+namespace Revit_PowerBI_Viewer
+{
+    public class Application : IExternalApplication
+    {
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+        public Result OnStartup(UIControlledApplication application)
+        { 
+            RibbonPanel panel = RibbonPanel(application);
+            string thisAsemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            if (panel.AddItem(new PushButtonData("PowerBI Viewer", "PowerBI Viewer", thisAsemblyPath, "Revit_PowerBI_Viewer.Command")) is PushButton button)
+            {
+                button.ToolTip = "View PowerBI reports in Revit";
+
+                Uri uri = new Uri(Path.Combine(Path.GetDirectoryName(thisAsemblyPath), "Resources", "jacobian.ico"));
+                BitmapImage bitmap = new BitmapImage(uri);
+                button.LargeImage = bitmap;
+
+            }
+            else
+            {
+                Debug.WriteLine("Failed to add PowerBI Viewer button to the ribbon panel.");
+            }
+
+            return Result.Succeeded;
+        }
+
+        public RibbonPanel RibbonPanel(UIControlledApplication a)
+        {
+            string tab = "Michal_S";
+            RibbonPanel ribbonPanel = null;
+
+            try
+            {
+                a.CreateRibbonTab(tab);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                // Tab already exists, do nothing
+            }
+
+            try
+            {     
+                a.CreateRibbonPanel(tab, "PowerBI Viewer");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                // Panel already exists, do nothing
+            }
+
+            List<RibbonPanel> panels = a.GetRibbonPanels(tab);
+            foreach (RibbonPanel p in panels.Where(p => p.Name == "PowerBI Viewer"))
+            {
+                ribbonPanel = p;            
+            }
+
+            return ribbonPanel;
+        }
+    }
+    
+}
---
+++ Revit_PowerBI_Viewer/Revit_PowerBI_Viewer/Command.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+using System.Windows.Forms;
+
+namespace Revit_PowerBI_Viewer
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command :IExternalCommand
+    {
+        
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+       
+        // Show a simple message box as a placeholder for the Power BI viewer functionality
+        MessageBox.Show("Plugin Created");
+        return Result.Succeeded;
+        
+            
+        }
+    }
+}
